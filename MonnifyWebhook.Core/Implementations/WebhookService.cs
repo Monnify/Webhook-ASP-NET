e113acefc36d4d0c00a80aa0102e33f4cef1690f
toc-dev <tochukwusage4@gmail.com>
2024-09-04 17:48:15 +0100

    tested and currently working. Monnify webhook notification tests with C# Asp.Net

---
+++ MonnifyWebhook.Core/Implementations/WebhookService.cs
@@ -0,0 +1,13 @@
+﻿using MonnifyWebhook.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonnifyWebhook.Core.Implementations
+{
+    public class WebhookService:IWebhookService
+    {
+    }
+}
